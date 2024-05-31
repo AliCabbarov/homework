81b1730a988f3930a70c1952b192b1f2d8d40bde
AliCabbarov <ali.cab@div.edu.az>
2024-06-01 02:55:00 +0400

    added nginx double ms request

--- docker-task/src/main/java/com/example/controller/Controller.java
+++ docker-task/src/main/java/com/example/controller/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping()
     public String get(){
-        return "welcome the controller";
+        return "welcome from customer ms";
     }
 }
