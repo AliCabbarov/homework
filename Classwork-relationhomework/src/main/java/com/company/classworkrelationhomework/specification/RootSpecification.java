b4e4013bdfd1d3781ab9ff32639134af01e75832
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-22 16:02:16 +0400

    Create RootSpecification.java

---
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/specification/RootSpecification.java
@@ -0,0 +1,76 @@
+package com.company.classworkrelationhomework.specification;
+
+import com.company.classworkrelationhomework.model.dto.specification.SearchCriteria;
+import com.company.classworkrelationhomework.model.enums.SearchOperation;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.NonNull;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+@Service()
+@Scope(scopeName = "request",proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class RootSpecification<T> implements Specification<T> {
+    private final List<SearchCriteria> list;
+
+    public RootSpecification() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(SearchCriteria criteria) {
+        list.add(criteria);
+    }
+
+    @Override
+    public Predicate toPredicate(@NonNull Root<T> root,@NonNull CriteriaQuery<?> query,@NonNull CriteriaBuilder builder) {
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        for (SearchCriteria criteria : list) {
+            if (criteria.getOperation().equals(SearchOperation.GREATER_THAN)) {
+                predicates.add(builder.greaterThan(
+                        root.get(criteria.getKey()), criteria.getValue().toString()));
+            } else if (criteria.getOperation().equals(SearchOperation.LESS_THAN)) {
+                predicates.add(builder.lessThan(
+                        root.get(criteria.getKey()), criteria.getValue().toString()));
+            } else if (criteria.getOperation().equals(SearchOperation.GREATER_THAN_EQUAL)) {
+                predicates.add(builder.greaterThanOrEqualTo(
+                        root.get(criteria.getKey()), criteria.getValue().toString()));
+            } else if (criteria.getOperation().equals(SearchOperation.LESS_THAN_EQUAL)) {
+                predicates.add(builder.lessThanOrEqualTo(
+                        root.get(criteria.getKey()), criteria.getValue().toString()));
+            } else if (criteria.getOperation().equals(SearchOperation.NOT_EQUAL)) {
+                predicates.add(builder.notEqual(
+                        root.get(criteria.getKey()), criteria.getValue()));
+            } else if (criteria.getOperation().equals(SearchOperation.EQUAL)) {
+                predicates.add(builder.equal(
+                        root.get(criteria.getKey()), criteria.getValue()));
+            } else if (criteria.getOperation().equals(SearchOperation.MATCH)) {
+                predicates.add(builder.like(
+                        builder.lower(root.get(criteria.getKey())),
+                        "%" + criteria.getValue().toString().toLowerCase() + "%"));
+            } else if (criteria.getOperation().equals(SearchOperation.MATCH_END)) {
+                predicates.add(builder.like(
+                        builder.lower(root.get(criteria.getKey())),
+                        criteria.getValue().toString().toLowerCase() + "%"));
+            } else if (criteria.getOperation().equals(SearchOperation.MATCH_START)) {
+                predicates.add(builder.like(
+                        builder.lower(root.get(criteria.getKey())),
+                        "%" + criteria.getValue().toString().toLowerCase()));
+            } else if (criteria.getOperation().equals(SearchOperation.IN)) {
+                predicates.add(builder.in(root.get(criteria.getKey())).value(criteria.getValue()));
+            } else if (criteria.getOperation().equals(SearchOperation.NOT_IN)) {
+                predicates.add(builder.not(root.get(criteria.getKey())).in(criteria.getValue()));
+            }
+        }
+        
+
+        return builder.and(predicates.toArray(new Predicate[0]));
+    }
+}
