1d2c2ec609c2d1a679a9cc60365cfd7278b2ca42
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-22 16:02:08 +0400

    Update ProductServiceImpl.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/impl/ProductServiceImpl.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/impl/ProductServiceImpl.java
@@ -3,7 +3,8 @@ package com.company.classworkrelationhomework.service.impl;
 import com.company.classworkrelationhomework.mapper.ProductMapper;
 import com.company.classworkrelationhomework.model.dto.request.ProductRequestDto;
 import com.company.classworkrelationhomework.model.dto.response.ProductResponseDto;
-import com.company.classworkrelationhomework.model.dto.specification.ProductSpecificationDto;
+import com.company.classworkrelationhomework.model.dto.specification.SearchCriteria;
+import com.company.classworkrelationhomework.model.dto.specification.product.ProductSpecificationDto;
 import com.company.classworkrelationhomework.model.entity.Category;
 import com.company.classworkrelationhomework.model.entity.Product;
 import com.company.classworkrelationhomework.model.enums.ProductSort;
@@ -13,6 +14,7 @@ import com.company.classworkrelationhomework.repository.ProductRepository;
 import com.company.classworkrelationhomework.service.CategoryService;
 import com.company.classworkrelationhomework.service.ProductService;
 import com.company.classworkrelationhomework.specification.ProductSpecification;
+import com.company.classworkrelationhomework.specification.RootSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
@@ -31,6 +33,7 @@ public class ProductServiceImpl implements ProductService {
     private final ProductMapper productMapper;
     private final CategoryService categoryService;
     private final ProductSpecification productSpecification;
+    private final RootSpecification<Product> rootSpecification;
 
     @Override
     public ResponseEntity<ProductResponseDto> create(ProductRequestDto dto) {
@@ -63,12 +66,12 @@ public class ProductServiceImpl implements ProductService {
     public ResponseEntity<List<ProductResponseDto>> productBySpecification(ProductSpecificationDto dto) {
         Specification<Product> specification;
         if (dto != null) specification = productSpecification.hasName(dto.getName())
-                    .and(productSpecification.greaterThanPrice(dto.getInitialPrice()))
-                    .and(productSpecification.lessThanPrice(dto.getSecondPrice()))
-                    .and(productSpecification
-                            .sorted(dto.getProductSort() == null ? ProductSort.ID : dto.getProductSort(),dto.getIsAsc() != null ? dto.getIsAsc() : true));
+                .and(productSpecification.greaterThanPrice(dto.getInitialPrice()))
+                .and(productSpecification.lessThanPrice(dto.getSecondPrice()))
+                .and(productSpecification
+                        .sorted(dto.getProductSort() == null ? ProductSort.ID : dto.getProductSort(), dto.getIsAsc() != null ? dto.getIsAsc() : true));
 
-        else specification = productSpecification.sorted(ProductSort.ID,true);
+        else specification = productSpecification.sorted(ProductSort.ID, true);
 
 
         List<ProductResponseDto> response = productRepository.findAll(specification).stream()
@@ -83,5 +86,14 @@ public class ProductServiceImpl implements ProductService {
         return productMapper.map(getById(id));
     }
 
-
+    @Override
+    public ResponseEntity<List<ProductResponseDto>> searchByCriteria(List<SearchCriteria> dto) {
+        for (SearchCriteria searchCriteria : dto) {
+            rootSpecification.add(searchCriteria);
+        }
+        List<ProductResponseDto> response = productRepository.findAll(rootSpecification).stream()
+                .map(productMapper::map)
+                .toList();
+        return ResponseEntity.ok(response);
+    }
 }
