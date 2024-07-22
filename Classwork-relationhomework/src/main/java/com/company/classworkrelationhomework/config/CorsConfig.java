4f2ed43694a88629dec76214d723649516f97ed9
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-22 14:12:06 +0400

    update service

---
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/config/CorsConfig.java
@@ -0,0 +1,37 @@
+package com.company.classworkrelationhomework.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("http://localhost:63342"); // for specific origin
+         corsConfiguration.addAllowedOriginPattern("*"); // for all origins
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:63342") // for specific origin
+                 .allowedOriginPatterns("*") // for all origins
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
---
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/specification/ProductSpecification.java
@@ -0,0 +1,35 @@
+package com.company.classworkrelationhomework.specification;
+
+import com.company.classworkrelationhomework.model.entity.Product;
+import com.company.classworkrelationhomework.model.enums.ProductSort;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@SuppressWarnings("all")
+public class ProductSpecification {
+
+    public Specification<Product> hasName(String name) {
+        return (root, query, criteriaBuilder) -> name != null ? criteriaBuilder.equal(root.get("name"),name) : criteriaBuilder.conjunction();
+    }
+    public Specification<Product> greaterThanPrice(BigDecimal price) {
+        return (root, query, criteriaBuilder) -> price != null ? criteriaBuilder.greaterThan(root.get("price"),price) : criteriaBuilder.conjunction();
+    }
+    public Specification<Product> lessThanPrice(BigDecimal price) {
+        return (root, query, criteriaBuilder) -> price != null ? criteriaBuilder.lessThan(root.get("price"),price) : criteriaBuilder.conjunction();
+    }
+    public Specification<Product> none() {
+        return ((root, query, criteriaBuilder) -> criteriaBuilder.conjunction());
+    }
+    public Specification<Product> sorted(@NotNull ProductSort sort, Boolean isAsc) {
+        return (root, query, criteriaBuilder) -> {
+            if (isAsc) query.orderBy(criteriaBuilder.asc(root.get(sort.getParam())));
+            else query.orderBy(criteriaBuilder.desc(root.get(sort.getParam())));
+            return criteriaBuilder.conjunction();
+        };
+    }
+
+}
