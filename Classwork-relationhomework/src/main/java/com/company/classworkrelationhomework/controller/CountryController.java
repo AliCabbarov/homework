217b84531945602179f23d1d4fd6ab58268afdbd
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-23 14:27:39 +0400

    Create CountryController.java

---
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/controller/CountryController.java
@@ -0,0 +1,25 @@
+package com.company.classworkrelationhomework.controller;
+
+import com.company.classworkrelationhomework.model.dto.request.CountryRequestDto;
+import com.company.classworkrelationhomework.model.dto.response.CountryResponseDto;
+import com.company.classworkrelationhomework.service.CountryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+@RequiredArgsConstructor
+public class CountryController {
+    private final CountryService countryService;
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody CountryRequestDto dto){
+        return countryService.create(dto);
+    }
+    @GetMapping
+    public ResponseEntity<List<CountryResponseDto>> getAll(){
+        return countryService.getAll();
+    }
+}
