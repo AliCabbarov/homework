069b19b6a3730627e81afe693eaa881a7318917c
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-23 17:42:37 +0400

    Update ClassworkRelationHomeworkApplication.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/ClassworkRelationHomeworkApplication.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/ClassworkRelationHomeworkApplication.java
@@ -1,12 +1,18 @@
 package com.company.classworkrelationhomework;
 
+import com.company.classworkrelationhomework.client.CountriesClient;
+import com.company.classworkrelationhomework.model.dto.response.client.CountriesClientResponseDto;
+import com.company.classworkrelationhomework.model.dto.response.client.CountryClientResponseDto;
+import com.company.classworkrelationhomework.model.entity.Country;
 import com.company.classworkrelationhomework.model.entity.Product;
+import com.company.classworkrelationhomework.repository.CountryRepository;
 import com.company.classworkrelationhomework.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 import java.util.List;
 import java.util.function.Function;
@@ -14,7 +20,10 @@ import java.util.function.Function;
 @SpringBootApplication
 @RequiredArgsConstructor
 @EnableCaching
+@EnableFeignClients
 public class ClassworkRelationHomeworkApplication implements CommandLineRunner {
+    private final CountriesClient client;
+    private final CountryRepository countryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ClassworkRelationHomeworkApplication.class, args);
@@ -23,5 +32,6 @@ public class ClassworkRelationHomeworkApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+
     }
 }
