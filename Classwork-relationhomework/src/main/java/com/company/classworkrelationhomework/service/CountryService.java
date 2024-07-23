4a45009d067c711911ed1fce766f6f8cff174f3b
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-23 14:28:06 +0400

    Create CountryService.java

---
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/CountryService.java
@@ -0,0 +1,13 @@
+package com.company.classworkrelationhomework.service;
+
+import com.company.classworkrelationhomework.model.dto.request.CountryRequestDto;
+import com.company.classworkrelationhomework.model.dto.response.CountryResponseDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CountryService {
+    ResponseEntity<Void> create(CountryRequestDto dto);
+
+    ResponseEntity<List<CountryResponseDto>> getAll();
+}
