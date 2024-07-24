58eddf38caab43dac5a77b3ca88b02d116227f10
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-24 16:20:07 +0400

    Update CartServiceImpl.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/impl/CartServiceImpl.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/impl/CartServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class CartServiceImpl implements CartService {
 
