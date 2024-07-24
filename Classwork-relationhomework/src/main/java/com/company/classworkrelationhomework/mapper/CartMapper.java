69c3b166426154f32d89a4cc4661583f1081aae9
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-24 16:14:48 +0400

    Update CartMapper.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/mapper/CartMapper.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/mapper/CartMapper.java
@@ -5,6 +5,7 @@ import com.company.classworkrelationhomework.model.entity.Cart;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
+@SuppressWarnings("all")
 public interface CartMapper {
     CartResponseDto map(Cart cart);
 }
