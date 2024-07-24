d46e18f543e4fd7f076ea8253aad082266f649db
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-24 16:14:58 +0400

    Update CountryRepository.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/repository/CountryRepository.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/repository/CountryRepository.java
@@ -2,9 +2,17 @@ package com.company.classworkrelationhomework.repository;
 
 import com.company.classworkrelationhomework.model.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
+    @Override
+    @Query("delete from Country where id =:id")
+    @Modifying
+    @Transactional
+    void deleteById(Long id);
 }
