f4f4ebde9bcde2eb613a2c68b86ab87098fcfae7
Ali Cabbarov <ali.cab@div.edu.az>
2024-07-30 10:31:22 +0400

    Update CountryMapper.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/mapper/CountryMapper.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/mapper/CountryMapper.java
@@ -4,10 +4,13 @@ import com.company.classworkrelationhomework.model.dto.request.CountryRequestDto
 import com.company.classworkrelationhomework.model.dto.response.CountryResponseDto;
 import com.company.classworkrelationhomework.model.entity.Country;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
-@SuppressWarnings("all")
 public interface CountryMapper {
+    @Mapping(target = "id",ignore = true)
+    @Mapping(target = "iso2",ignore = true)
+    @Mapping(target = "iso3",ignore = true)
     Country map(CountryRequestDto dto);
     CountryResponseDto map(Country country);
 }
