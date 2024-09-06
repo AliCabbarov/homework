b7f819fc4dc94ba17291a4707816abb6c6db6b63
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:45:57 +0400

    Update CartResponseDto.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/CartResponseDto.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/CartResponseDto.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(value = JsonInclude.Include.NON_NULL)
-public class CartResponseDto {
+public class CartResponseDto implements Serializable {
     private Long id;
     private String name;
     private List<ProductResponseDto> products;
