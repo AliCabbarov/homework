9dc5be5b70042011a509f8a6fb645f0afa9894b8
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:46:02 +0400

    Update ProductDetailResponseDto.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/ProductDetailResponseDto.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/ProductDetailResponseDto.java
@@ -2,12 +2,17 @@ package com.company.classworkrelationhomework.model.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SecondaryRow;
+
+import java.io.Serializable;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class ProductDetailResponseDto {
+@NoArgsConstructor
+public class ProductDetailResponseDto implements Serializable {
     private long id;
     private String color;
     private String imageUrl;
