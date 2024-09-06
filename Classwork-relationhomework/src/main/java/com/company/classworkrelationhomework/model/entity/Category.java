917aea32668d06b0e2a6f5c4264813b6ebc00495
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:46:10 +0400

    Update Category.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/entity/Category.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/entity/Category.java
@@ -4,14 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import java.io.Serializable;
 
 
 @Entity
 @Getter
 @Setter
-public class Category {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
