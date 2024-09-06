7f334162c6d14410f7f1143806787b87afbcf92c
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:46:19 +0400

    Update CartService.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/CartService.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/CartService.java
@@ -2,14 +2,20 @@ package com.company.classworkrelationhomework.service;
 
 import com.company.classworkrelationhomework.model.dto.request.CartRequestDto;
 import com.company.classworkrelationhomework.model.dto.response.CartResponseDto;
+import com.company.classworkrelationhomework.model.dto.response.ProductResponseDto;
+import com.company.classworkrelationhomework.model.entity.Cart;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 public interface CartService {
     ResponseEntity<CartResponseDto> create(CartRequestDto dto);
 
-    ResponseEntity<Void> addProduct(Long id, Long productId);
+    ResponseEntity<CartResponseDto> addProduct(Long id, Long productId);
 
     ResponseEntity<Void> delete(Long id, Long productId);
 
     ResponseEntity<CartResponseDto> get(Long id);
+
+    ResponseEntity<Void> deleteCart(Long id);
 }
