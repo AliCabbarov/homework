368afc9e3a70edb8c17030b73f25002e49809f6e
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:46:04 +0400

    Update ProductResponseDto.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/ProductResponseDto.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/ProductResponseDto.java
@@ -3,13 +3,16 @@ package com.company.classworkrelationhomework.model.dto.response;
 import com.company.classworkrelationhomework.model.dto.request.ProductDetailRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
-public class ProductResponseDto {
+@NoArgsConstructor
+public class ProductResponseDto implements Serializable {
     private long id;
     private String name;
     private String description;
