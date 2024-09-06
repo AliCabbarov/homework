fa9d572e437de181fb48dd0a52f0e9d6d52fc6e4
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:45:50 +0400

    Update RedisConfig.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/config/RedisConfig.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/config/RedisConfig.java
@@ -1,15 +1,16 @@
 package com.company.classworkrelationhomework.config;
 
+import com.company.classworkrelationhomework.model.dto.response.CartResponseDto;
 import com.company.classworkrelationhomework.model.entity.Country;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 import java.util.List;
@@ -25,6 +26,7 @@ public class RedisConfig {
         template.afterPropertiesSet();
         return template;
     }
+
     @Bean
     public RedisTemplate<String, List<Country>> countryList() {
         RedisTemplate<String, List<Country>> template = new RedisTemplate<>();
@@ -34,16 +36,36 @@ public class RedisConfig {
         return template;
     }
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    LettuceConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(6379);
+        return new LettuceConnectionFactory(configuration);
+    }
+    @Bean
+    public RedisTemplate<Long, CartResponseDto> productList() {
+        RedisTemplate<Long, CartResponseDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer("UTF-8"));
+        template.setKeySerializer(new GenericJackson2JsonRedisSerializer("UTF-8"));
+        template.afterPropertiesSet();
+        return template;
     }
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
         RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(60))
                 .disableCachingNullValues();
+
+        RedisCacheConfiguration productCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues();
+
+
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(cacheConfiguration)
+                .withCacheConfiguration("cart", productCacheConfig)
                 .build();
     }
+
 }
