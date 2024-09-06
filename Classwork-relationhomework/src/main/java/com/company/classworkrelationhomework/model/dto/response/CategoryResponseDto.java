c169914be5648cbaa541c4dc3bf4f1f8e1a40b1b
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:45:59 +0400

    Update CategoryResponseDto.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/CategoryResponseDto.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/model/dto/response/CategoryResponseDto.java
@@ -2,12 +2,16 @@ package com.company.classworkrelationhomework.model.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class CategoryResponseDto {
+@NoArgsConstructor
+public class CategoryResponseDto implements Serializable {
     private long id;
     private String name;
 }
