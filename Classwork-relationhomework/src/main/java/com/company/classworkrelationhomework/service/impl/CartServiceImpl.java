c4d48ff537e459e09f381269c2c8bba2e1edb361
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:46:21 +0400

    Update CartServiceImpl.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/impl/CartServiceImpl.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.company.classworkrelationhomework.service.impl;
 
 import com.company.classworkrelationhomework.mapper.CartMapper;
+import com.company.classworkrelationhomework.mapper.ProductMapper;
 import com.company.classworkrelationhomework.model.dto.request.CartRequestDto;
 import com.company.classworkrelationhomework.model.dto.response.CartResponseDto;
 import com.company.classworkrelationhomework.model.entity.Cart;
@@ -9,10 +10,13 @@ import com.company.classworkrelationhomework.repository.CartRepository;
 import com.company.classworkrelationhomework.service.CartService;
 import com.company.classworkrelationhomework.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 
 @Service
@@ -22,20 +26,33 @@ public class CartServiceImpl implements CartService {
     private final CartRepository cartRepository;
     private final ProductService productService;
     private final CartMapper cartMapper;
+    private final RedisTemplate<Long, CartResponseDto> redisTemplate;
+    private final ProductMapper productMapper;
 
     @Override
     public ResponseEntity<CartResponseDto> create(CartRequestDto dto) {
-        Cart cart = cartRepository.save(Cart.builder().name(dto.getName()).build());
-        return ResponseEntity.ok(new CartResponseDto(cart.getId(), cart.getName()));
+        Cart cart = cartRepository.save(Cart.builder().name(dto.getName()).products(new HashSet<>()).build());
+
+        CartResponseDto cartResponseDto = cartMapper.map(cart);
+
+        redisTemplate.opsForValue().set(cart.getId(), cartResponseDto);
+
+        return ResponseEntity.ok(cartResponseDto);
     }
 
     @Override
-    @Transactional
-    public ResponseEntity<Void> addProduct(Long id, Long productId) {
+    public ResponseEntity<CartResponseDto> addProduct(Long id, Long productId) {
         Cart cart = getCart(id);
         Product product = productService.getById(productId);
         cart.getProducts().add(product);
-        return ResponseEntity.ok().build();
+
+        Cart savedCart = cartRepository.save(cart);
+
+        CartResponseDto map = cartMapper.map(savedCart);
+
+        redisTemplate.opsForValue().set(cart.getId(), map);
+
+        return ResponseEntity.ok(map);
     }
 
     @Override
@@ -44,12 +61,31 @@ public class CartServiceImpl implements CartService {
         Cart cart = getCart(id);
         Product product = productService.getById(productId);
         cart.getProducts().remove(product);
+
+        CartResponseDto cartResponseDto = cartMapper.map(cart);
+        redisTemplate.opsForValue().set(cart.getId(),cartResponseDto);
+
         return ResponseEntity.ok().build();
     }
 
     @Override
+    @Transactional
     public ResponseEntity<CartResponseDto> get(Long id) {
-        return ResponseEntity.ok(cartMapper.map(getCart(id)));
+        CartResponseDto responseDto = redisTemplate.opsForValue().get(id);
+        if (responseDto == null) {
+            Cart cart = getCart(id);
+            responseDto = cartMapper.map(cart);
+            redisTemplate.opsForValue().set(id, responseDto);
+        }
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @Override
+    public ResponseEntity<Void> deleteCart(Long id) {
+        Cart cart = getCart(id);
+        cartRepository.delete(cart);
+        redisTemplate.delete(id);
+        return ResponseEntity.ok().build();
     }
 
     private Cart getCart(Long id) {
