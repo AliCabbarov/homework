46880e2a69eb99fc53b93f80e10dd5fed813d4fc
Ali Cabbarov <ali.cab@div.edu.az>
2024-09-06 14:45:54 +0400

    Update CartController.java

--- Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/controller/CartController.java
+++ Classwork-relationhomework/src/main/java/com/company/classworkrelationhomework/controller/CartController.java
@@ -2,10 +2,13 @@ package com.company.classworkrelationhomework.controller;
 
 import com.company.classworkrelationhomework.model.dto.request.CartRequestDto;
 import com.company.classworkrelationhomework.model.dto.response.CartResponseDto;
+import com.company.classworkrelationhomework.model.dto.response.ProductResponseDto;
 import com.company.classworkrelationhomework.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/carts")
@@ -21,15 +24,21 @@ public class CartController {
     @PostMapping("/{cartId}/product/{productId}")
     public ResponseEntity<Void> addProduct(@PathVariable Long cartId,
                                            @PathVariable Long productId) {
-        return cartService.addProduct(cartId,productId);
+        cartService.addProduct(cartId,productId);
+        return ResponseEntity.ok().build();
     }
     @DeleteMapping("/{cartId}/product/{productId}")
-    public ResponseEntity<Void> delete(@PathVariable Long cartId,
+    public ResponseEntity<Void> deleteFromCart(@PathVariable Long cartId,
                                        @PathVariable Long productId){
         return cartService.delete(cartId,productId);
     }
     @GetMapping("/{id}")
     public ResponseEntity<CartResponseDto> get(@PathVariable Long id){
         return cartService.get(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return cartService.deleteCart(id);
     }
 }
